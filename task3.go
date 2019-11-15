15a7861dfa2dcc457288957855f1984ca2b50f75
Vladislav Kulizhenko <37963994+COOLizh@users.noreply.github.com>
2019-11-15 16:06:42 +0300

    added new methos named End(), Perimeter(), Area()

---
+++ task3.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// Point indicates the coordinates of a point on a plane
+type Point struct {
+	x, y int
+}
+
+// Square contains the necessary data to describe the square
+type Square struct {
+	start Point
+	a     uint
+}
+
+// End сalculates the end coordinates of a square
+func (s Square) End() (uint, uint) {
+	return uint(s.start.x) + s.a, uint(s.start.y) + s.a
+}
+
+// Perimeter calculates the perimeter of a square
+func (s Square) Perimeter() uint {
+	return 4 * s.a
+}
+
+// Area calculates the area of the square
+func (s Square) Area() uint {
+	return s.a * s.a
+}
+
+func main() {
+	s := Square{Point{1, 1}, 5}
+	fmt.Println(s.End())
+	fmt.Println(s.Perimeter())
+	fmt.Println(s.Area())
+}
