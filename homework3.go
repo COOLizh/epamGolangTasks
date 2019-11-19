8abb3da27fbfd8e836ade3b632659c88d0b8de36
Vladislav Kulizhenko <37963994+COOLizh@users.noreply.github.com>
2019-11-20 01:32:53 +0300

    Completed third homework

---
+++ homework3.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"unicode/utf8"
+)
+
+/*Task 1 : average([...]int) float64 function that returns
+an average value of array (sum / N)*/
+func average(array [6]int) float64 {
+	var sum int
+	for _, item := range array {
+		sum += item
+	}
+	return float64(sum) / float64(len(array))
+}
+
+/*Task 2.1 : function that returns the longest word from the slice
+of strings (the first if there are more than one).*/
+func max(array []string) string {
+	if array == nil {
+		return "There is no strings in slice!"
+	}
+	maxString := array[0]
+	for i := 1; i < len(array); i++ {
+		if utf8.RuneCountInString(array[i]) > utf8.RuneCountInString(maxString) {
+			maxString = array[i]
+		}
+	}
+	return maxString
+}
+
+/*Task 2.2 : function that returns the copy of the original
+slice in reverse order. The type of elements is int64.*/
+func reverse(array []int64) []int64 {
+	slice := make([]int64, 0, len(array))
+	for i := len(array) - 1; i >= 0; i-- {
+		slice = append(slice, array[i])
+	}
+	return slice
+}
+
+/*Task 3 : function that prints map values sorted in order of increasing keys.*/
+func printSorted(m map[int]string) {
+	keys := make([]int, 0, len(m))
+	for i := range m {
+		keys = append(keys, i)
+	}
+	sort.Ints(keys)
+	ans := make([]string, 0, len(m))
+	for _, key := range keys {
+		ans = append(ans, m[key])
+	}
+	fmt.Println(ans)
+}
+
+func main() {
+	/*Task 1 : Implement average([...]int) float64 function
+	that returns an average value of array (sum / N)*/
+	var firstTaskArray [6]int
+	for i := 1; i <= 6; i++ {
+		firstTaskArray[i-1] = i
+	}
+	fmt.Println(average(firstTaskArray))
+
+	/*Task 2.1 : Write max([]string) string function that returns
+	the longest word from the slice of strings (the first if there are more than one).*/
+	fmt.Println(max([]string{"one", "two", "three"}))
+	fmt.Println(max([]string{"one", "two"}))
+	var firstSlice []string
+	fmt.Println(max(firstSlice))
+
+	/*Task 2.2 : Write reverse([]int64) []int64 function that returns
+	the copy of the original slice in reverse order. The type of elements is int64.*/
+	var secondSlice = []int64{1, 2, 5, 15}
+	fmt.Println(reverse(secondSlice))
+	fmt.Println(secondSlice)
+
+	/*Task 3 : Implement printSorted(map[int]string)function
+	that prints map values sorted in order of increasing keys.*/
+	var firstM = map[int]string{2: "a", 0: "b", 1: "c"}
+	var secondM = map[int]string{10: "aa", 0: "bb", 500: "cc"}
+	printSorted(firstM)
+	printSorted(secondM)
+}
